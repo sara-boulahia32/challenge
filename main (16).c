f75e7c72692b3e379d374b45d11a644e85b10ef3
Sara BouLahia <144260111+sara-boulahia32@users.noreply.github.com>
2024-09-05 10:06:55 +0200

    Add files via upload

---
+++ main (16).c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby,
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int i, n, p, somme, compt;
+    
+    
+    while (1){
+        printf("entier positif se terminant par 0 , 0 pour passer au calcul  ");
+        scanf("%d", &n);
+        if(n==0){
+            break;
+        }else if(n>0){
+            if (n%10==0){
+                
+            somme+=n;
+        compt++;
+            }else{
+                printf("entrez entier positif se terminant par 0");
+            }
+        }else{
+            printf("positive seulement");
+        }
+       
+    }
+    printf("moyenne est %d ", somme/compt);
+    return 0;
+}
